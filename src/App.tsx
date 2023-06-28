b2e4097d13e3666a0a9ff612f21eea0c4e0cabcf
vladimir2412 <vova.osmin2@gmail.com>
2023-06-28 23:22:45 +0300

    adding useTheme hook

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,13 @@ import { Link } from 'react-router-dom';
 import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
 import { MainPageAsync } from './pages/MainPage/MainPage.async';
 import { Suspense } from 'react';
+import { useTheme } from './theme/useTheme';
+
 const App = () => {
+	const { theme, toggleTheme } = useTheme();
 	return (
-		<div className="app">
+		<div className={`app ${theme}`}>
+			<button onClick={() => toggleTheme()}>Toggle Theme</button>
 			<Link to="/">Home page</Link>
 			<Link to="/about">About page</Link>
 			<Suspense fallback={<h1>loading...</h1>}>
